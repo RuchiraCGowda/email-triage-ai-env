d3720e8093684ca44887f9fee48d4aaeb401b9c1
Ruchira C <232803031+RuchiraCGowda@users.noreply.github.com>
2026-04-03 20:11:18 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -5,19 +5,20 @@ from agent import Agent
 
 app = FastAPI()
 
-env = EmailEnv(mode="all")
-agent = Agent()
-
 class StepInput(BaseModel):
     email: str
 
 @app.post("/reset")
 def reset():
+    env = EmailEnv(mode="all")
     state = env.reset()
     return {"state": state}
 
 @app.post("/step")
 def step(input: StepInput):
+    env = EmailEnv(mode="all")
+    agent = Agent()
+
     action = agent.act(input.email)
     state, reward, done = env.step(action)
 
