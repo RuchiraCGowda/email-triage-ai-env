9f646fefb0ca5b22b068f0d122f99812ac52407c
Ruchira C <232803031+RuchiraCGowda@users.noreply.github.com>
2026-04-03 19:37:55 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,4 +1,3 @@
-import os
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -10,22 +9,17 @@ app = FastAPI()
 env = EmailEnv(mode="all")
 agent = Agent()
 
-
 class StepInput(BaseModel):
     email: str
-
 
 @app.post("/reset")
 def reset():
     state = env.reset()
     return {"state": state}
 
-
 @app.post("/step")
 def step(input: StepInput):
-    email = input.email
-
-    action = agent.act(email)
+    action = agent.act(input.email)
     state, reward, done = env.step(action)
 
     return {
