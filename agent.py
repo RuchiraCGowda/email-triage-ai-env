1efef041ecdba74b2f737d5ee0baaefda12dedc2
Ruchira C <232803031+RuchiraCGowda@users.noreply.github.com>
2026-04-18 01:46:54 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -2,7 +2,7 @@ class Agent:
     def act(self, email):
         email = email.lower()
 
-        if "free" in email or "win" in email or "offer" in email:
+        if "free" in email or "win" in email or "offer" in email: 
             return "spam"
         elif "urgent" in email or "important" in email:
             return "urgent"
