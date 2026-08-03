91de39e5543e0a2a61164761efa7cabe92b1f8b1
Ruchira C <232803031+RuchiraCGowda@users.noreply.github.com>
2026-08-04 00:29:25 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -3,7 +3,7 @@ def run():
 
     print(f"[START] task={task_name}", flush=True)
 
-    # Example step (you can connect your logic here)
+    # Example step (you can connect your logic here)    
     step = 1
     reward = 0.5
     print(f"[STEP] step={step} reward={reward}", flush=True)
